d476f68c88c528667ead5342a9185334fd463dc0
Raflidafrian <raflidafrian220@gmail.com>
2025-06-14 00:27:02 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COBA GIT REMOTE</title>
+    < /title>
+</head>
+
+<body>
+
+    <h1>hello world1</h1>
+
+</body>
+
+</html>
